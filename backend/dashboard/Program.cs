8338d8907929dbc6bc865e9ae57e969d06ddd2ad
Ivan Sychev <ivansycev0@gmail.com>
2026-08-11 00:03:31 +0500

    [ref] replace OpenApi with Swashbuckle

--- backend/dashboard/Program.cs
+++ backend/dashboard/Program.cs
@@ -11,21 +11,23 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         
-        builder.Services.AddOpenApi();
         builder.Services.AddSignalR();
+        builder.Services.AddCors();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
         builder.Services.AddSingleton<ISystemMetricsProvider, SystemMetricsProvider>();
         builder.Services.AddSingleton<SystemMetricsCache>();
         builder.Services.AddHostedService<CacheUpdateService>();
         builder.Services.AddHostedService<MetricsBroadcastService>();
+        builder.Services.AddSingleton<MetricsHubTracker>();
         
         
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
+        
         if (app.Environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.MapOpenApi();
+            app.UseSwagger();
             app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "dashboard v1"));
             app.UseCors(policy => policy
                 .WithOrigins("http://localhost:5173")
