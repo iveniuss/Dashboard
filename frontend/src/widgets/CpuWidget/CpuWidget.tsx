a9a434d79ed8a01c1496994dca1c03cbbd5e51bb
Ivan Sychev <ivansycev0@gmail.com>
2026-08-16 16:21:31 +0500

    [ref] some linting updates

--- frontend/src/widgets/CpuWidget/CpuWidget.tsx
+++ frontend/src/widgets/CpuWidget/CpuWidget.tsx
@@ -1,16 +1,18 @@
-import { ProgressCircle } from "@chakra-ui/react"
+import { Box, ProgressCircle } from "@chakra-ui/react";
 
 interface CpuWidgetProps {
   usage: number;
 }
 
-export const CpuWidget = ({usage}: CpuWidgetProps) => {
-
-  return <ProgressCircle.Root value={usage}>
-    <ProgressCircle.Circle>
-      <ProgressCircle.Track/>
-      <ProgressCircle.Range strokeLinecap={"round"}/>
-    </ProgressCircle.Circle>
-  </ProgressCircle.Root>
-
-}
+export const CpuWidget = ({ usage }: CpuWidgetProps) => {
+  return (
+    <Box>
+      <ProgressCircle.Root value={usage}>
+        <ProgressCircle.Circle>
+          <ProgressCircle.Track />
+          <ProgressCircle.Range strokeLinecap={"round"} />
+        </ProgressCircle.Circle>
+      </ProgressCircle.Root>
+    </Box>
+  );
+};
