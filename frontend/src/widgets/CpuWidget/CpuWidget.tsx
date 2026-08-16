aee4e342de95c0648c23669f5b3dcad356477d10
Ivan Sychev <ivansycev0@gmail.com>
2026-08-16 22:56:23 +0500

    [feat] update CpuWidget

--- frontend/src/widgets/CpuWidget/CpuWidget.tsx
+++ frontend/src/widgets/CpuWidget/CpuWidget.tsx
@@ -1,4 +1,4 @@
-import { Box, ProgressCircle } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, ProgressCircle, Text } from "@chakra-ui/react";
 
 interface CpuWidgetProps {
   usage: number;
@@ -6,12 +6,28 @@ interface CpuWidgetProps {
 
 export const CpuWidget = ({ usage }: CpuWidgetProps) => {
   return (
-    <Box>
-      <ProgressCircle.Root value={usage}>
-        <ProgressCircle.Circle>
+    <Box
+      p="1rem"
+      bg={"gray.muted"}
+      w={"10rem"}
+      h={"10rem"}
+      position="relative"
+      rounded="4xl"
+      boxShadow="md"
+    >
+      <ProgressCircle.Root value={usage} colorPalette="green">
+        <ProgressCircle.Circle
+          css={{ "--size": "8rem", "--thickness": "0.75rem" }}
+        >
           <ProgressCircle.Track />
           <ProgressCircle.Range strokeLinecap={"round"} />
         </ProgressCircle.Circle>
+        <AbsoluteCenter>
+          <Text textStyle={"2xl"} fontWeight={"bold"}>
+            {usage}
+            <Text textStyle={"sm"} verticalAlign={"baseline"} as={"span"}>%</Text>
+          </Text>
+        </AbsoluteCenter>
       </ProgressCircle.Root>
     </Box>
   );
