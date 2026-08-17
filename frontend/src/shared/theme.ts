0fc83607d0277a6d5bc35adad2c6bd2d899bf86f
Ivan Sychev <ivansycev0@gmail.com>
2026-08-18 00:33:24 +0500

    [frontend|fix] make working colors semantic tokens

--- frontend/src/shared/theme.ts
+++ frontend/src/shared/theme.ts
@@ -1,36 +1,29 @@
-import {
-  createSystem,
-  defaultConfig,
-  defineConfig,
-} from "@chakra-ui/react";
+import { createSystem, defaultConfig, defineConfig } from "@chakra-ui/react";
 import { progressCircleRecipe } from "@/shared/recipes/ProgressCircle.recipe";
-
 
 const config = defineConfig({
   globalCss: {
     body: {
-      bg: "bg.main",
+      bg: "bg.main"
     },
   },
   theme: {
     slotRecipes: {
       progressCircle: progressCircleRecipe,
     },
-    semanticTokens:{
-      colors:{
-        "bg.main":{
-          value:{
-            base: "bg"
-          }
+    semanticTokens: {
+      colors: {
+        bg: {
+          main: {
+            value: { base: "{white}", _dark:"{colors.gray.900}" },
+          },
+          card: {
+            value: { base: "{colors.bg.muted}", _dark:"{colors.gray.800}" },
+          },
         },
-        "bg.card":{
-          value:{
-            base: "bg.subtle"
-          }
-        }
-      }
-    }
+      },
+    },
   },
 });
 
-export default createSystem(defaultConfig, config)
+export default createSystem(defaultConfig, config);
