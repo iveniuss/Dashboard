c45f94ddc6f57acaf05965e083f42558fd3a81ef
Ivan Sychev <ivansycev0@gmail.com>
2026-08-19 00:05:12 +0500

    [frontend|feat] add swap usage to MemoryWidget

--- frontend/src/widgets/MemoryWidget.tsx
+++ frontend/src/widgets/MemoryWidget.tsx
@@ -13,21 +13,19 @@ interface MemoryWidgetProps {
   total: number;
   used: number;
   usedWithCache: number;
+  swapUsed:number;
 }
 
 export const MemoryWidget = (memInfo: MemoryWidgetProps) => {
   return (
-    <WidgetBox
-      cells={[2, 1]}
-      title={"Memory Load"}
-    >
+    <WidgetBox cells={[2, 1]} title={"Memory Load"}>
       <Flex
         position="relative"
         justifyContent={"space-between"}
         alignItems={"center"}
       >
         <Box>
-          <DataList.Root orientation="horizontal">
+          <DataList.Root orientation="horizontal" gap="1">
             <DataList.Item>
               <DataList.ItemLabel minW="5rem">Total</DataList.ItemLabel>
               <DataList.ItemValue>
@@ -44,6 +42,12 @@ export const MemoryWidget = (memInfo: MemoryWidgetProps) => {
               <DataList.ItemLabel minW="5rem">Cached</DataList.ItemLabel>
               <DataList.ItemValue>
                 <FormatByte value={memInfo.usedWithCache - memInfo.used} />
+              </DataList.ItemValue>
+            </DataList.Item>
+            <DataList.Item>
+              <DataList.ItemLabel minW="5rem">Swap</DataList.ItemLabel>
+              <DataList.ItemValue>
+                <FormatByte value={memInfo.swapUsed} />
               </DataList.ItemValue>
             </DataList.Item>
           </DataList.Root>
