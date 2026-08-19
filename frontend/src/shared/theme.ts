0ee7526fdf7dd69a48f34d93defecc1dc76c5fc7
iveniuss <ivansycev0@gmail.com>
2026-08-19 14:14:48 +0500

    [frontend|feat] add Nunito font

--- frontend/src/shared/theme.ts
+++ frontend/src/shared/theme.ts
@@ -4,21 +4,27 @@ import { progressCircleRecipe } from "@/shared/recipes/ProgressCircle.recipe";
 const config = defineConfig({
   globalCss: {
     body: {
-      bg: "bg.main"
-    },
+      bg: "bg.main",
+    }
   },
   theme: {
     slotRecipes: {
       progressCircle: progressCircleRecipe,
     },
+    tokens: {
+      fonts: {
+        heading: { value: "'Nunito', sans-serif" },
+        body: { value: "'Nunito', sans-serif" },
+      },
+    },
     semanticTokens: {
       colors: {
         bg: {
           main: {
-            value: { base: "{white}", _dark:"{colors.gray.900}" },
+            value: { base: "{white}", _dark: "{colors.gray.900}" },
           },
           card: {
-            value: { base: "{colors.bg.muted}", _dark:"{colors.gray.800}" },
+            value: { base: "{colors.bg.muted}", _dark: "{colors.gray.800}" },
           },
         },
       },
